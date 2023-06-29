6048f8c9bbff8258da4c97c9219bb76fcc9d20c4
Alex Kosova <whovian2504@yandex.ru>
2023-06-29 03:02:55 +0300

    пр-13

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const routes = require('./routes/routes');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-
 app.use((req, res, next) => {
   req.user = {
     _id: '649b58714042af9b50f73492',
