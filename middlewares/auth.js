a32ba6c2f4df7214131d34735e66236d1a8888fe
Alex Kosova <whovian2504@yandex.ru>
2023-07-05 00:29:21 +0300

    пр-14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,22 +3,19 @@ const AuthError = require('../errors/AuthError');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || authorization.startWith('Bearer')) {
+  if (!authorization || !authorization.startWith('Bearer')) {
     next(new AuthError('Необходимо авторизоваться'));
   }
 
-  const token = req.cookies.jwt;
+  const token = authorization.replace('Bearer', '');
+  let payload;
 
-  if (!token) {
-    next(new AuthError('Необходимо авторизоваться'));
-  }
   try {
-    const payload = jwt.verify(token, 'secretKey');
-    req.user = payload;
+    payload = jwt.verify(token, 'secretKey');
   } catch (err) {
     next(new AuthError('Необходимо авторизоваться'));
   }
-
+  req.user = payload;
   next();
 };
 
